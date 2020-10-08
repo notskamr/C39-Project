d5c702288ac7372a450487724ec84dc0d9303758
notskamr <66314478+notskamr@users.noreply.github.com>
2020-10-08 16:52:06 +0530

    1

--- js/form.js
+++ js/form.js
@@ -1,23 +1,64 @@
 class Form{
     constructor(){
-       this.input = createInput("Name");
+       this.inputText = createElement('h2')
+       this.errorText = createElement('h2')
+       this.input = createInput(" ", 'search');
        this.button = createButton('Play');
        this.greeting = createElement('h2');
-       this.title = createElement('h2');
+       this.title = createElement('h1');
        this.reset = createButton('Reset');
     }
+
+    inputCheck(inputValue) {
+        inputValue == inputValue.toString()
+        if(typeof inputValue == 'string' && inputValue != " " ) {
+            this.button.mousePressed(() => {
+                this.input.hide();
+                this.button.hide()
+                this.inputText.hide();
+                player.name = this.input.value();
+                playerCount += 1;
+                player.index = playerCount;
+                player.update();
+                player.updateCount(playerCount);
+                this.greeting.html("Hello " + player.name)
+                this.greeting.position(400,250);
+                this.greeting.style('color', 'white');
+                this.greeting.style('font-size', '100px');
+            });
+
+        
+            
+        }
+
+        else {
+            this.errorText.position(550,500)
+            this.errorText.html("Not a valid name!");
+        }
+    }
+    
     hide() {
         this.greeting.hide();
         this.button.hide();
         this.input.hide();
         this.title.hide();
+        this.inputText.hide()
     }
     display() {
-        this.title.html("FRUIT CATCHER");
+
+        
+        this.inputText.position(550,300)
+        
+        this.title.html("SYNTHWAVE");
         this.title.position(350, 50);
         this.title.style('font-size', '70px');
         this.title.style('color', 'skyblue');
+       
+        this.inputText.html("Enter a name!");
+        
+
         this.input.position(550,400);
+        this.inputText.style('color', 'lime');
         this.input.style('width', '200px');
         this.input.style('height', '20px');
         this.input.style('background', 'lavender');
@@ -30,19 +71,7 @@ class Form{
         this.reset.style('height', '30px');
         this.reset.style('background', 'lightpink');
 
-        this.button.mousePressed(() => {
-            this.input.hide();
-            this.button.hide();
-            player.name = this.input.value();
-            playerCount += 1;
-            player.index = playerCount;
-            player.update();
-            player.updateCount(playerCount);
-            this.greeting.html("Hello " + player.name)
-            this.greeting.position(400,250);
-            this.greeting.style('color', 'white');
-            this.greeting.style('font-size', '100px');
-        });
+       inputCheck('input')
 
         this.reset.mousePressed(() => {
             player.updateCount(0);
